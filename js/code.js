1aa28ccddd01538f7e0861e17e498c95d6581e8c
Michał Kuwik <Michal7074@poczta.fm>
2019-06-04 20:04:56 +0200

    edits for index.css is finished, in index.js add new listeners for animate piece of front

--- js/code.js
+++ js/code.js
@@ -3,10 +3,17 @@
 const btnStart = document.querySelector('button.btn');
 const headerDiv = document.querySelector('.header');
 const jinDiv = document.querySelector('.jin');
+const rpanel = document.querySelector('.panelright');
+const overLay = document.querySelector('.overlay');
 
 btnStart.addEventListener('click',() => {
   setTimeout(function(){
     headerDiv.style.display = 'none';
-  },2000);
-  setTimeout(() => jinDiv.style.display = 'block',2200);
+  },1000);
+  setTimeout(() => jinDiv.style.display = 'block',1500);
+  setTimeout(() => rpanel.classList.add('click'),2000);
+  setTimeout(() => overLay.classList.add('show'),2600);
+
+ 
 });
+
