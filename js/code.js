8f29629005e6af6fb89c7cb8d06c624d1f7cb2a3
Michał Kuwik <Michal7074@poczta.fm>
2019-06-04 18:32:41 +0200

    add DOM Eelements in code.js and add event listener on Startbutton

--- js/code.js
+++ js/code.js
@@ -1,1 +1,12 @@
-console.log('podpięte');
+//GET DOM ELEMENTS
+
+const btnStart = document.querySelector('button.btn');
+const headerDiv = document.querySelector('.header');
+const jinDiv = document.querySelector('.jin');
+
+btnStart.addEventListener('click',() => {
+  setTimeout(function(){
+    headerDiv.style.display = 'none';
+  },2000);
+  setTimeout(() => jinDiv.style.display = 'block',2200);
+});
