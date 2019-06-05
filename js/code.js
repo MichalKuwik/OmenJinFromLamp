4695356096ce636611fb700643f87ec573d0f6ae
Michał Kuwik <Michal7074@poczta.fm>
2019-06-05 17:27:04 +0200

    small changes in media queries of font-size for h1 tag, in index.js add many DOM elements and add eventListeners for animations on click to addOmen button

--- js/code.js
+++ js/code.js
@@ -5,6 +5,9 @@ const headerDiv = document.querySelector('.header');
 const jinDiv = document.querySelector('.jin');
 const rpanel = document.querySelector('.panelright');
 const overLay = document.querySelector('.overlay');
+const btnShow = document.querySelector('button.showOmen');
+const output = document.querySelector('.output');
+const lamp = document.querySelector('.img-div');
 
 btnStart.addEventListener('click',() => {
   setTimeout(function(){
@@ -13,7 +16,10 @@ btnStart.addEventListener('click',() => {
   setTimeout(() => jinDiv.style.display = 'block',1500);
   setTimeout(() => rpanel.classList.add('click'),2000);
   setTimeout(() => overLay.classList.add('show'),2600);
-
- 
 });
 
+btnShow.addEventListener('click',() => {
+  setTimeout(()=> lamp.classList.add('run'),10);
+  setTimeout(()=> output.innerHTML = 'Pierwsza wróżba!',1400);
+  setTimeout(()=> lamp.classList.remove('run'),1400);
+});
