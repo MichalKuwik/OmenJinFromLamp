550e13cbb2ab1122b29e2820ec3aae4e36838212
Michał Kuwik <Michal7074@poczta.fm>
2019-06-06 22:32:01 +0200

    add finish of code on code.js- this code clesr input after click addOmen - button

--- js/code.js
+++ js/code.js
@@ -24,10 +24,17 @@ btnStart.addEventListener('click',() => {
 });
 
 btnShow.addEventListener('click',() => {
+
+  //method to random show omen
   const randomOmenShow = () => {
+    //get single index
     const index = Math.floor(Math.random() * omenArray.length);
     output.innerHTML = `${omenArray[index]}`;
+    let newValue = input.value;
+    
   }
+
+  
   setTimeout(()=> lamp.classList.add('run'),10);
   setTimeout(randomOmenShow,1400);
   setTimeout(()=> lamp.classList.remove('run'),1400);
@@ -35,6 +42,7 @@ btnShow.addEventListener('click',() => {
   
 });
 
+//array with defalut omens at start
 const omenArray = [
   'Weź się do roboty!',
   'Będziesz łysy za kilka lat',
@@ -42,30 +50,30 @@ const omenArray = [
   'Poznasz swoją miłość'
 ];
 
+//btn add new Omen
 btnAdd.addEventListener('click',()=> {
-  
-  
-
   setTimeout(()=> info.classList.add('showInfo'),1000);
   setTimeout(()=> {
     infoCounter.classList.add('showCount');
-    
-      
-
-    },500);
+    },2500);
     let number = omenArray.length;
     infoCounter.innerHTML = `Ilość wróżb: ${number}`;
-    console.log(omenArray);
+    // console.log(omenArray);
 
     let newValue = input.value;
     
+    //push new omen to array
     if(input.value){
       omenArray.push(newValue);
       newValue = '';
     }
+    setTimeout(()=> {
+      info.classList.remove('showInfo');
+      },4500);
 });
 
-
+//END event listener ,when click this button, input.value is empy string
+btnAdd.addEventListener('click',()=> input.value = '');
 
 
 
